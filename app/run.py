d7b50a3c615eb98f1f010b06b1062624108dce8d
Andrey Kats <andreykats@gmail.com>
2023-02-02 18:29:05 -0500

    added local S3 container for testing

--- app/run.py
+++ app/run.py
@@ -3,6 +3,8 @@ from os import environ
 
 # Set the environment variables for the local database
 environ["DB_HOST"] = "http://localhost:8000"
+environ["S3_IMAGE_BUCKET"] = "vandal-images-bucket-dev"
+environ["S3_HOST"] = "http://localhost.localstack.cloud:4566"
 
 if __name__ == "__main__":
     uvicorn.run("src.main:api", host="127.0.0.1", port=8080, reload=True)
